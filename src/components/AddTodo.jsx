87bc515717b76b427e6e5e0e5c9dd3ea9ffc0c1e
Jeevan Gaudel <jatingaudel01@gmail.com>
2024-04-03 17:30:14 +1100

    created to do list using react

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,13 @@
+function AddTodo(){
+return  <div class="container text-center">
+    <div class="row">
+<div class="col-6"><input type="text" placeholder="Enter tofo here" /></div>
+<div class="col-4"><input type="date" /></div>
+<div class="col-2"><button class="btn btn-primary">Add</button></div>
+</div>
+</div>
+
+
+}
+
+export default AddTodo;
